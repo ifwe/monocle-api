56e028446178fd329ac32d1de5533cb1e7b2127f
Hector Virgen <hvirgen@ifwe.co>
2015-11-16 21:19:39 -0800

    Improves nested resources
    Allows the top-level handler to return nested resources without invoking additonal API calls.

--- test/lib/Resource_test.js
+++ test/lib/Resource_test.js
@@ -20,19 +20,16 @@ describe('Resource', function() {
         });
 
         it('contains $id', function() {
-            var representation = this.resource.toRepresentation();
-            representation.should.have.property('$id', this.id);
+            this.resource.should.have.property('$id', this.id);
         });
 
         it('contains $expires', function() {
-            var representation = this.resource.toRepresentation();
-            representation.should.have.property('$expires', this.expires);
+            this.resource.should.have.property('$expires', this.expires);
         });
 
         it('contains data', function() {
-            var representation = this.resource.toRepresentation();
-            representation.should.have.property('foo', this.data.foo);
-            representation.should.have.property('bar', this.data.bar);
+            this.resource.should.have.property('foo', this.data.foo);
+            this.resource.should.have.property('bar', this.data.bar);
         });
     });
 });
