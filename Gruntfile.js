6ba217f34851acb64d4278791f1fd612a5e270a2
Hector Virgen <hvirgen@ifwe.co>
2017-05-17 17:07:21 -0700

    Improves auto-generated documentation
    Auto-generated documentation now has the following improvements:
    
    - Only one resource is shown at a time, eliminating the need to scroll endlessly on large APIs
    - Resource list navigation now shows route pattern and HTTP verbs
    - Mobile-friendly view

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,58 +48,8 @@ module.exports = function(grunt) {
             }
         },
 
-        // Server-side mocha tests
-        mochaTest: {
-            // Runs all tests
-            test: {
-                options: {
-                    require: TEST_RUNNER,
-                    reporter: 'spec',
-                    ui: 'bdd',
-                    timeout: 200,
-                    recursive: true,
-                    clearRequireCache: true
-                },
-                src: [ALL_TESTS]
-            },
-
-            // Instruments code for reporting test coverage
-            instrument: {
-                options: {
-                    require: TEST_RUNNER,
-                    reporter: 'spec',
-                    ui: 'bdd',
-                    timeout: 200,
-                    recursive: true,
-                },
-                src: [ALL_TESTS]
-            },
-
-            // Reports test coverage
-            coverage: {
-                options: {
-                    require: TEST_RUNNER,
-                    reporter: 'html-cov',
-                    ui: 'bdd',
-                    timeout: 200,
-                    recursive: true,
-                    quiet: true,
-                    captureFile: 'test/coverage.html'
-                },
-                src: [ALL_TESTS]
-            }
-        },
-
         // Watches filesystem for changes to run tasks automatically
         watch: {
-            test: {
-                options: { spawn: false },
-                files: [
-                    'lib/**/*.js',
-                    'test/**/*.js'
-                ],
-                tasks: ['mochaTest:test']
-            },
             docco: {
                 options: { spawn: false },
                 files: ['lib/**/*.js'],
--- test/test_runner.js
+++ test/test_runner.js
@@ -3,9 +3,6 @@ var path = require('path');
 // Use constant to help with resolving path to lib code within test files
 GLOBAL.LIB_DIR = path.join(process.cwd(), 'lib');
 
-// Set up in-place instrumentation for code coverage
-require('blanket')({ pattern: LIB_DIR });
-
 // Set up sinon
 GLOBAL.sinon = require('sinon');
 
