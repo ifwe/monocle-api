ebef5a892b8258ee1bf909483bc32ec69ff9891c
Tyson Andre <tandre@themeetgroup.com>
2018-01-04 13:24:54 -0800

    Use `global` instead of deprecated `GLOBAL`

--- test/test_runner.js
+++ test/test_runner.js
@@ -1,14 +1,14 @@
 var path = require('path');
 
 // Use constant to help with resolving path to lib code within test files
-GLOBAL.LIB_DIR = path.join(process.cwd(), 'lib');
+global.LIB_DIR = path.join(process.cwd(), 'lib');
 
 // Set up sinon
-GLOBAL.sinon = require('sinon');
+global.sinon = require('sinon');
 
 // Set up chai
-GLOBAL.chai = require('chai');
+global.chai = require('chai');
 chai.should();
-GLOBAL.assert = chai.assert;
-GLOBAL.expect = chai.expect;
+global.assert = chai.assert;
+global.expect = chai.expect;
 chai.config.includeStack = true;
